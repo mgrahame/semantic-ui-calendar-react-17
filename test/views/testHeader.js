20b11f70e4e52f7060d224587246641584a063e8
Michael Grahame <mgrahame@gmail.com>
2021-03-27 19:54:30 -0600

    chore: update dependencies

--- test/views/testHeader.js
+++ test/views/testHeader.js
@@ -1,6 +1,6 @@
 import { assert } from 'chai';
 import * as Enzyme from 'enzyme';
-import * as Adapter from 'enzyme-adapter-react-16';
+import * as Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import moment from 'moment';
 import {
   shallow,
